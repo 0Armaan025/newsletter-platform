99fb6f5c3831385c9ced286cd6b9cd12c2483bdf
0Armaan025 <armaan33000@gmail.com>
2024-03-21 10:25:51 +0530

    feature highlight done

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -2,13 +2,17 @@ import Header from "@/shared/widgets/header/header";
 import React from "react";
 import Banner from "./features/Banner";
 import Branding from "./features/Branding";
+import Benefits from "./features/Benefits";
+import FeatureHighlight from "./features/Feature.highlight";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner />
-      <Branding/>
+      <Branding />
+      <Benefits />
+      <FeatureHighlight />
     </div>
   );
 };
