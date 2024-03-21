49cf5685978368ea17920652cf2b03c4e854a62b
0Armaan025 <armaan33000@gmail.com>
2024-03-21 22:42:26 +0530

    fix

---
+++ src/shared/widgets/dashboard/sidebar/dashboard.items.tsx
@@ -0,0 +1,5 @@
+const DashboardItems = ({ bottomContent }: { bottomContent?: boolean }) => {
+  return <div></div>;
+};
+
+export default DashboardItems;
--- src/shared/widgets/dashboard/sidebar/dashboard.sidebar.tsx
+++ src/shared/widgets/dashboard/sidebar/dashboard.sidebar.tsx
@@ -2,13 +2,20 @@
 import { ICONS } from "@/shared/utils/icons";
 import { useUser } from "@clerk/nextjs";
 import React from "react";
+import DashboardItems from "./dashboard.items";
 
 const DashboardSideBar = () => {
   const { user } = useUser();
   return (
-    <div className="p-2 flex items-center bg-[#f5f5f5] roundeed">
-      <span className="text-2xl">{ICONS.home}</span>
-      <h5 className="pl-2 pt-1 capitalize">{user?.username} Newsletter</h5>
+    <div className="p-2">
+      <div className="p-2 flex items-center bg-[#f5f5f5] roundeed">
+        <span className="text-2xl">{ICONS.home}</span>
+        <h5 className="pl-2 pt-1 capitalize">{user?.username} Newsletter</h5>
+      </div>
+      <div>
+        <DashboardItems />
+        {/* 1:47:22 */}
+      </div>
     </div>
   );
 };
