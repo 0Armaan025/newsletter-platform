5cc10627168fe810c71925c28cdce73f11510240
0Armaan025 <armaan33000@gmail.com>
2024-03-22 16:50:51 +0530

    new

--- src/shared/widgets/dashboard/sidebar/dashboard.items.tsx
+++ src/shared/widgets/dashboard/sidebar/dashboard.items.tsx
@@ -1,5 +1,107 @@
+import { sideBarBottomItems, sideBarItems } from "@/app/configs/constants";
+import useRouteChange from "@/shared/hooks/useRouteChange";
+import { ICONS } from "@/shared/utils/icons";
+import { useClerk } from "@clerk/nextjs";
+import Link from "next/link";
+import { redirect, usePathname } from "next/navigation";
+import SideBarFooterLogo from "./sidebar.footer.logo";
+import { useEffect } from "react";
+
 const DashboardItems = ({ bottomContent }: { bottomContent?: boolean }) => {
-  return <div></div>;
+  const { activeRoute, setActiveRoute } = useRouteChange();
+  const { signOut, user } = useClerk();
+  const pathName = usePathname();
+
+  const LogOutHandler = () => {
+    signOut();
+    redirect("/sign-up");
+  };
+
+  useEffect(() => {
+    setActiveRoute(pathName);
+  }, [pathName, setActiveRoute]);
+
+  return (
+    <>
+      {!bottomContent ? (
+        <>
+          {sideBarItems.map((item: DashboardSideBarTypes, index: number) => (
+            <Link
+              key={index}
+              href={item.url}
+              className="p-2 py-5 flex items-center"
+            >
+              <span
+                className={`text-3xl mr-2 ${
+                  item.url === activeRoute && "text-[#463bbd]"
+                } `}
+              >
+                {item.icon}
+              </span>
+              <span
+                className={`text-xl mr-2 ${
+                  item.url === activeRoute && "text-[#463bbd]"
+                }`}
+              >
+                {item.title}
+              </span>
+            </Link>
+          ))}
+        </>
+      ) : (
+        <>
+          {sideBarBottomItems.map(
+            (item: DashboardSideBarTypes, index: number) => (
+              <Link
+                href={
+                  item.url === "/"
+                    ? `/subscribe?username=${user?.username}`
+                    : item.url
+                }
+                key={index}
+                className="p-2 py-5 flex items-center"
+              >
+                <span
+                  className={`text-3xl mr-2 ${
+                    item.url === activeRoute && "text-[#463bbd]"
+                  } `}
+                >
+                  {item.icon}
+                </span>
+                <span
+                  className={`text-xl mr-2 ${
+                    item.url === activeRoute && "text-[#463bbd]"
+                  }`}
+                >
+                  {item.title}
+                </span>
+              </Link>
+            )
+          )}
+          {/* sign out
+           */}
+          <div
+            className="p-2 py-5 flex items-center cursor-pointer border-b"
+            onClick={LogOutHandler}
+          >
+            <span className="text-3xl mr-2">{ICONS.logOut}</span>
+            <span className="text-xl">Sign Out!</span>
+          </div>
+          {/* footer
+           */}
+          <br />
+          <br />
+          <div className="w-full flex justify-center cursor-pointer">
+            <SideBarFooterLogo />
+          </div>
+          <p className="text-sm text-center pt-5 pb-10">
+            {" "}
+            ©️ 2024 Armaan, Inc. All rights reserved.
+          </p>
+        </>
+      )}
+    </>
+  );
 };
 
 export default DashboardItems;
